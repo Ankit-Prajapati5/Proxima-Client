e15d4447b87e14965524175e1424ae685126c20e
Ankit Prajapati <ankitpjlko143@gmail.com>
2026-01-26 08:45:33 +0530

    Updated

--- src/features/api/baseApi.js
+++ src/features/api/baseApi.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 export const baseApi = fetchBaseQuery({
-  // baseUrl: "http://localhost:5000/api/v1",
-  baseUrl: import.meta.env.VITE_API_BASE_URL,
+  baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1",
   credentials: "include", // 🔥 VERY IMPORTANT (cookie auth)
 });
